65fcbbaf70325738019631865b0aa18eb5e37916
hayesla <hayesla@tcd.ie>
2020-04-28 20:28:18 -0400

    fixing

--- goes_event_list.py
+++ goes_event_list.py
@@ -5,6 +5,7 @@ from sunpy.net import hek
 from sunpy.time import parse_time
 import csv
 
+
 def get_goes_event_list(tstart, tend, filename='goes_event_list.csv', goes_class_filter=None):
     """
     Function to get the GOES flare event list and save it as a csv.
@@ -18,7 +19,7 @@ def get_goes_event_list(tstart, tend, filename='goes_event_list.csv', goes_class
     filename : ~str, optional
         name of csv file to save results to
     goes_class_filter : ~str, optional
-        minimum GOES class flare to search for, only returns flares 
+        minimum GOES class flare to search for, only returns flares
         with a goes class greater than given
 
     Returns
@@ -29,28 +30,27 @@ def get_goes_event_list(tstart, tend, filename='goes_event_list.csv', goes_class
     --------
     >>> t_start = '2014-01-01 00:00'
     >>> t_end = '2014-01-28 23:59'
-    >>> goes_class_filter = 'C1.0' 
+    >>> goes_class_filter = 'C1.0'
     # searches for flares greater than C1.0 between t_start and t_end
     >>> get_goes_event_list(t_start, t_end, goes_class_filter=goes_class_filter)
     """
-    
+
     client = hek.HEKClient()
     event_type = 'FL'
 
-
     if goes_class_filter:
-        result=client.search(hek.attrs.Time(tstart,tend),hek.attrs.EventType(event_type),hek.attrs.FL.GOESCls >= goes_class_filter,hek.attrs.OBS.Observatory == 'GOES')
+        result = client.search(hek.attrs.Time(tstart, tend), hek.attrs.EventType(event_type), hek.attrs.FL.GOESCls >= goes_class_filter, hek.attrs.OBS.Observatory == 'GOES')
     else:
-        result=client.search(hek.attrs.Time(tstart,tend),hek.attrs.EventType(event_type),hek.attrs.OBS.Observatory == 'GOES')
+        result = client.search(hek.attrs.Time(tstart, tend), hek.attrs.EventType(event_type), hek.attrs.OBS.Observatory == 'GOES')
 
-    goes_event_list=[]
+    goes_event_list = []
 
     for r in result:
-        goes_event={}
-        tmpstring=str.split(str(r['event_starttime']),'T')
-        date=tmpstring[0]
+        goes_event = {}
+        tmpstring = str.split(str(r['event_starttime']), 'T')
+        date = tmpstring[0]
         goes_event['event_date'] = date
-        goes_event['start_time'] =parse_time(r['event_starttime'])
+        goes_event['start_time'] = parse_time(r['event_starttime'])
         goes_event['peak_time'] = parse_time(r['event_peaktime'])
         goes_event['end_time'] = parse_time(r['event_endtime'])
         goes_event['goes_class'] = str(r['fl_goescls'])
@@ -58,18 +58,7 @@ def get_goes_event_list(tstart, tend, filename='goes_event_list.csv', goes_class
 
     with open(filename, 'w') as csvfile:
         fieldnames = goes_event_list[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for i in range(len(goes_event_list)):
             writer.writerow(goes_event_list[i])
-    
-
-t_start = '2014-01-01 00:00'
-t_end = '2019-02-28 23:59'
-goes_class_filter = 'C1.0'
-
-get_goes_event_list(t_start, t_end, goes_class_filter=goes_class_filter)
-
-
-    
-
--- magno_codes/download_BIRR_data.py
+++ magno_codes/download_BIRR_data.py
@@ -1,73 +1,70 @@
 import urllib
 from pathlib import Path
-import sys
-sys.path.append('..')
 import utils
 from sunpy.time import parse_time
+import sys
+sys.path.append('..')
+
 
 def find_files(date):
-	"""
-	Function to list available files for Birr magnetometer data. The filename
-	depends on the start seconds of the data which can change for certain days
-	hence actually finding the file name is required as it cannot necessarily be
-	predicted
+    """
+    Function to list available files for Birr magnetometer data. The filename
+    depends on the start seconds of the data which can change for certain days
+    hence actually finding the file name is required as it cannot necessarily be
+    predicted
 
-	Parameters
-	----------
-	date : ~astropy.time.Time or datetime.datetime
-		time to search for
+    Parameters
+    ----------
+    date : ~astropy.time.Time or datetime.datetime
+        time to search for
 
-	Returns
-	-------
-	list of files
-	"""
+    Returns
+    -------
+    list of files
+    """
+    base_url = 'http://data.rosseobservatory.ie/data/'
 
+    date_base_url = base_url + date.strftime('%Y/%m/%d/magnetometer/txt/')
 
-	base_url = 'http://data.rosseobservatory.ie/data/'
+    files = utils.list_path_files(date_base_url, 'txt')
 
-	date_base_url = base_url + date.strftime('%Y/%m/%d/magnetometer/txt/')
-
-	files = utils.list_path_files(date_base_url, 'txt')
-
-	return files
-
+    return files
 
 
 def search_data_and_download(date, path=None):
-	"""
-	Function to search for and download magnetometer data from Birr
+    """
+    Function to search for and download magnetometer data from Birr
 
-	Parameters
-	----------
-	date : ~str
-		date to parse and search for data, should be in form acceptable
-		to `sunpy.time.parse_time()`
+    Parameters
+    ----------
+    date : ~str
+        date to parse and search for data, should be in form acceptable
+        to `sunpy.time.parse_time()`
 
-	path : ~str, optional
-		path to save the file to, if not given it will save to cwd.
+    path : ~str, optional
+        path to save the file to, if not given it will save to cwd.
 
-	Returns
-	-------
-	check : ~str`
-		returns a string to inform if data has downloaded or not
+    Returns
+    -------
+    check : ~str`
+        returns a string to inform if data has downloaded or not
 
-	"""
+    """
+    date = parse_time(date)
 
-	date = parse_time(date)
+    files = find_files(date)
+    if len(files) < 0:
+        return 'no files found'
+    else:
+        print('downloading now..')
 
-	files = find_files(date)
-	if len(files)<0:
-		return 'no files found'
-	else:
-		print('downloading now..')
+    if path is not None:
+        filename = Path(path).joinpath(files[0].split('/')[-1])
+    else:
+        filename = files[0].split('/')[-1]
 
-	if path is not None:
-		filename = Path(path).joinpath(files[0].split('/')[-1])
-	else:
-		filename = files[0].split('/')[-1]
-
-	urllib.request.urlretrieve(files[0], filename=filename)
-	if Path(filename).exists():
-		return 'files downloaded'
-	else:
-		return 'error in download'
+    urllib.request.urlretrieve(files[0], filename=filename)
+    if Path(filename).exists():
+        return 'files downloaded'
+    else:
+        return 'error in download'
