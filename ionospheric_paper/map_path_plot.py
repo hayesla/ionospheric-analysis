2481e5a73d884b15db1272d877689aa8be7a55e7
Laura Hayes <hayesla@tcd.ie>
2021-04-13 13:27:02 +0100

    adding repo for paper

---
+++ ionospheric_paper/map_path_plot.py
@@ -0,0 +1,7 @@
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from sunpy.time import parse_time
+from sunpy.coordinates import frames
